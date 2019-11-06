deec478c02d81715e7a7276a419f084710bc430c
olesyamogil <olesya.mogil@gmail.com>
2019-11-06 13:48:08 +0200

    Updates

---
+++ precache-manifest.8adbd75d3b694c1161970759378c15ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05e80c1c316b8250496b36a395e0488",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "8c9144212e29dddfb50a",
+    "url": "/react_dynamic-list-of-todos/static/css/main.04c529fd.chunk.css"
+  },
+  {
+    "revision": "a9d548c836abc462eee7",
+    "url": "/react_dynamic-list-of-todos/static/js/2.f6a999b2.chunk.js"
+  },
+  {
+    "revision": "8c9144212e29dddfb50a",
+    "url": "/react_dynamic-list-of-todos/static/js/main.1f2f404f.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
